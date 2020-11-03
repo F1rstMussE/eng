5d3a77be16b92af5460cbf7ebf115ff84e8664a3
F1rstMussE <56113370+F1rstMussE@users.noreply.github.com>
2020-11-03 21:46:02 +0700

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,1 +1,15 @@
-
+#include "en.h"
+int main() {
+	SetConsoleCP(CP_UTF8);
+    SetConsoleOutputCP(CP_UTF8);
+	char Text[20][40] = {0};
+	int d;
+	char ch;
+	int n,checks,cas;
+	cas = load(Text);
+	
+	printf("Hello it's your assistent for learn english!\nYou need write translate words which I'm print you");
+	
+	
+	return 0;
+}
